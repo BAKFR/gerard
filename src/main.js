8334f42877eb97473b9a2094d59b9a361ef7fd98
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2014-12-06 15:00:57 +0100

    Without user inpout, the ship slow down and stop.

--- src/main.js
+++ src/main.js
@@ -25,26 +25,38 @@ function create() {
     ship.addChild(graphics);
 
     game.physics.arcade.enable(ship, Phaser.Physics.ARCADE);
-    ship.body.velocity.x = 20;
 }
 
 function update() {
     var keyboard = game.input.keyboard;
+    var velocity = ship.body.velocity;
     var max_velocity = 250;
 
+    //The ship always slow down a little.
+    if (velocity.x < 2 && velocity.x > -2)
+        velocity.x = 0;
+    else
+        velocity.x += (velocity.x > 0 ? -2 : 2);
+    if (velocity.y < 2 && velocity.y > -2)
+        velocity.y = 0;
+    else
+        velocity.y += (velocity.y > 0 ? -2 : 2);
+
+    //Keyboard input
     if (keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        ship.body.velocity.x -= 5;
+        ship.body.velocity.x -= 7;
     }
     if (keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        ship.body.velocity.x += 5;
+        ship.body.velocity.x += 7;
     }
     if (keyboard.isDown(Phaser.Keyboard.UP)) {
-        ship.body.velocity.y -= 5;
+        ship.body.velocity.y -= 7;
     }
     if (keyboard.isDown(Phaser.Keyboard.DOWN)) {
-        ship.body.velocity.y += 5;
+        ship.body.velocity.y += 7;
     }
 
+    //Max velocity
     if (ship.body.velocity.x < - max_velocity)
         ship.body.velocity.x = - max_velocity;
     if (ship.body.velocity.x > max_velocity)
